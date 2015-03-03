f08a73552d82f5aedfab80150cd05d1902f2e719
Matthew Vander Hoff <matt@WebServer.(none)>
2015-03-03 23:32:24 +0000

    added a choice for what to sacrifice need to implement the sacrificeing

--- sacrifice.sh
+++ sacrifice.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-clear
-cowsay "take me demo gods"
-sleep 3
-clear
-cowsay "i am here to appease you"
-sleep 3
-clear
-cowsay -d "i am le dead"
-sleep 3
-clear
+# written by Matthew Vander Hoff
+# for use by any one who needs their demo to go smoothly 
+
+
+echo "how important is this demo?" #give desperate users their options
+echo "1) a chicken will do"
+echo "2) we need at least a sheep"
+echo "3) the demo gods are demanding a cow"
+echo "4) as we are open source a gnu will be sacrificed"
+echo "5) we need to kill a dragon~!"
+echo "6) the demo gods requrie virgin blood"
+
+read sacrifice # see what the poor sob needs to get his demo working
+
+case $sacrifice if # case statment, because no one wants 6 if statmnts 
+    1) $sacrifice = 'chicen';;
+    2) $sacrifice = 'sheep';;
+    3) $sacrifice = 'cow';;
+    4) $sacrifice = 'gnu';;
+    5) $sacrifice = 'dragon';;
+    6) $sacrifice = 'virgin';;
+esac
+
+
+
+#clear
+#cowsay "take me demo gods"
+#sleep 3
+#clear
+#cowsay "i am here to appease you"
+#sleep 3
+#clear
+#cowsay -d "i am le dead"
+#sleep 3
+#clear
