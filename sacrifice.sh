7803900221194718071473da34bdb0166901cc2d
Matthew Vander Hoff <matt@WebServer.(none)>
2015-03-04 01:14:22 +0000

    implemented sacrificing

--- sacrifice.sh
+++ sacrifice.sh
@@ -11,26 +11,45 @@ echo "4) as we are open source a gnu will be sacrificed"
 echo "5) we need to kill a dragon~!"
 echo "6) the demo gods requrie virgin blood"
 
-read sacrifice # see what the poor sob needs to get his demo working
+read sacrifice
+# see what the poor sob needs to get his demo working
 
-case $sacrifice if # case statment, because no one wants 6 if statmnts 
-    1) $sacrifice = 'chicen';;
-    2) $sacrifice = 'sheep';;
-    3) $sacrifice = 'cow';;
-    4) $sacrifice = 'gnu';;
-    5) $sacrifice = 'dragon';;
-    6) $sacrifice = 'virgin';;
+case $sacrifice in # case statment because no one wants 6 if statmnts 
+    1) sacrifice='chicen';;
+    2) sacrifice='sheep';;
+    4) sacrifice='gnu';;
+    5) sacrifice='dragon';;
+    6) sacrifice='virgin';;
 esac
 
+if [$sacrifice = 3]; then
+    cow
+else
+    notacow
+fi
 
+function cow {
+    clear
+    cowsay "take me demo gods"
+    sleep 3
+    clear
+    cowsay "i am here to appease you"
+    sleep 3
+    clear
+    cowsay -d "i am le dead"
+    sleep 3
+    clear
+}
 
-#clear
-#cowsay "take me demo gods"
-#sleep 3
-#clear
-#cowsay "i am here to appease you"
-#sleep 3
-#clear
-#cowsay -d "i am le dead"
-#sleep 3
-#clear
+fucntion notacow {
+    clear
+    cowsay -f $sacrifice "take me demo gods"
+    sleep 3
+    clear
+    cowsay -f $sacrifice "i am here to appease you"
+    sleep 3
+    clear
+    cowsay -f $sacrifice "i am le dead"
+    sleep 3
+    clear
+}
